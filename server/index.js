7d7ac16e08f54fc193926ea149345caf29b0cb63
Yogesh-10 <yogiyogesh100800@gmail.com>
2021-03-30 13:57:21 +0530

    creating routes and connect to DB

--- server/index.js
+++ server/index.js
@@ -1,3 +1,21 @@
 import express from 'express'
+import dotenv from 'dotenv'
 import cors from 'cors'
-import mongoose from 'mongoose'
+import connectDB from './config/db.js'
+import postRoutes from './routes/postRoutes.js'
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+app.use('/posts', postRoutes)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
